15e523c02d61e65f068ac3d0fd461f59e94673b8
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2018-07-09 16:10:31 +0530

    avgStdMean.py flatteningVector.py reshapingArrays.py transposingVector.py

--- chapter1/avgStdMean.py
+++ chapter1/avgStdMean.py
@@ -7,7 +7,22 @@ matrix = np.array([[1, 2, 3],
                    [7, 8, 9]])
 
 # Return mean
-print(np.mean(matrix))
+print("Mean \n",np.mean(matrix))
 
 # Return mean for column
-print(np.mean(matrix, axis=0))
+print("Mean by column \n",np.mean(matrix, axis=0))
+
+# Return mean for column
+print("Mean by row \n",np.mean(matrix, axis=1))
+
+# Return variance
+print("variance \n", np.var(matrix))
+
+# Return variance for column
+print("variance by column \n",np.var(matrix, axis=0))
+
+# Return variance for row
+print("variance by row \n",np.var(matrix, axis=1))
+
+# Return standard deviation
+print("deviation \n",np.std(matrix))

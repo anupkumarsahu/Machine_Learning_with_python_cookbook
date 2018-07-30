7082523f64a2b8da9e38c547294833cd4bcf71ef
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2018-07-30 09:55:48 +0530

    loadingDataset.py simulatedData.py

--- chapter3 Data Wrangling/titanicDataset.py
+++ chapter3 Data Wrangling/titanicDataset.py
@@ -10,3 +10,25 @@ dataframe = pd.read_csv(url)
 
 # Show first 5 rows
 print(dataframe.head(5))
+
+#Show dimensions
+print('dataframe: ',dataframe.shape)
+
+#Show statistics
+print('Statistics: ',dataframe.describe())
+
+# Select first row
+print(dataframe.iloc[0])
+
+# Select first three
+print(dataframe.iloc[1:4])
+
+# Show top two rows where column 'sex' is 'female'
+print(dataframe[dataframe['Sex']=='female'].head(5))
+
+# Rename column, show two rows
+dataframe.rename(columns={'PClass': 'Passenger Class'}).head(2)
+print(dataframe)
+
+# Calculate statistics
+print(dataframe['Age'].max())
